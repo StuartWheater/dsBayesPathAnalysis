ba8ef47077d9277c5f6deb573eaeabd47e0e938e
Stuart Wheater <stuart.wheater@arjuna.com>
2021-07-13 12:56:53 +0100

    Initial version

---
+++ R/hessSEM.assignDS.R
@@ -0,0 +1,12 @@
+#'
+#' @title Concatenates objects into a vector or list
+#' @description This function is similar to the R base function 'c'.
+#' @details Unlike the R base function 'c' on vector or list of certain 
+#' @param x a list which contains the the objects to concatenate.
+#' @return a vector or list
+#' @author DataSHIELD Development Team
+#' @export
+#'
+hessSEM.assignDS <- function (x) {
+  return(x)
+}
