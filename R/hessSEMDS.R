073b588ea07acf414f76f81f9ccba5c28ae34b98
Stuart Wheater <stuart.wheater@arjuna.com>
2021-07-19 14:41:27 +0100

    Added arguments

--- R/hessSEMDS.R
+++ R/hessSEMDS.R
@@ -3,10 +3,14 @@
 #' @description This function is similar to the R base function 'c'.
 #' @details Unlike the R base function 'c' on vector or list of certain 
 #' @param x a list which contains the the objects to concatenate.
+#' @param chains is the ...
+#' @param method is the ...
+#' @param seed is the ...
+#' @param iter is the ...
 #' @return a vector or list
 #' @author DataSHIELD Development Team
 #' @export
 #'
-hessSEMDS <- function (x) {
+hessSEMDS <- function (x, chains, method, seed, iter) {
   return(x)
 }
